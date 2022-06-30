941a2df36cb7b451242a900ae06f25aa6413adc8
adelani damilare <adelanidamilare@gmail.com>
2022-06-30 11:22:48 -0700

    h

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,9 @@
-#!/bin/bash
 #include<stdio.h>
 
-int main()
-
+int main(void)
 {
-char Mystr[] = ""Programming is like building a multilingual puzzle";
-
+char Mystr[] = (""Programming is like building a multilingual puzzle");
 puts(Mystr);
+return (0);
 
-return 0;
 }
-
-
