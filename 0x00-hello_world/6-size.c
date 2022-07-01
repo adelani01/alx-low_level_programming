62952aa8fd6a3ad6e785f9fe08cf5c61ea283da3
adelani damilare <adelanidamilare@gmail.com>
2022-06-30 20:58:06 -0700

    h

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf(“Size of char is: % d byte\n”, sizeof(char));
+printf(“Size of int is: % d byte\n”, sizeof(int));
+printf(“Size of long int is: % d byte\n”, sizeof(long int));
+printf(“Size of long long is: % d byte\n”, sizeof(long long));
+printf(“Size of float is: % d byte\n”, sizeof(float));
+return (0);
+}
