b62251f23311a8e39dea94ed1826ab4d045ab084
adelani damilare <adelanidamilare@gmail.com>
2022-07-02 03:01:51 -0700

    a

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,15 @@
  */
 int main(void)
 {
-printf("Size of a char: % d byte\n", sizeof(char));
-printf("Size of an int: % d byte\n", sizeof(int));
-printf("Size of a long int: % d byte\n", sizeof(long int));
-printf("Size of a long long int: % d byte\n", sizeof(long long int));
-printf("Size of a float: % d byte\n", sizeof(float));
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+printf("Size of a char: % d byte\n", sizeof(a));
+printf("Size of an int: % d byte\n", sizeof(b));
+printf("Size of a long int: % d byte\n", sizeof(c));
+printf("Size of a long long int: % d byte\n", sizeof(d));
+printf("Size of a float: % d byte\n", sizeof(e));
 return (0);
 }
