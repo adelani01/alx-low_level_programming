7386635a7028cad31b782013337ca573e366d88f
adelani damilare <adelanidamilare@gmail.com>
2022-07-04 09:02:02 -0700

    g

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ * Return: always return 0
+ *
+ */
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+for (ch = 'A'; ch <= 'Z'; ch++)
+putchar('\n');
+return (0);
+}
